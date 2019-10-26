489b5db28805599618c55bc1807eafbbad38f490
hwanggit <luw055@ucsd.edu>
2019-10-26 02:44:59 -0700

    new

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var postRouter = require('./routes/post');
-
 var app = express();
 
 // view engine setup
@@ -20,9 +15,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/alexa', postRouter);
+/* GET home page. */
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+app.post("/alexa", function(request, response) {
+  response.send('request');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/post.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.post("/alexa", function(request, response) {
-  console.log("request", request);
-  console.log("response", response);
-});
