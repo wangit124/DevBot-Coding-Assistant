580717ffc4a0ee81c8c48b22acb27ec059ace36b
hwanggit <luw055@ucsd.edu>
2019-10-26 12:08:50 -0700

    asdf

--- app.js
+++ app.js
@@ -3,28 +3,23 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const Alexa = require("ask-sdk-core");
-const { ExpressAdapter } = require("ask-sdk-express-adapter");
+const bodyParser = require('body-parser');
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('./public'));
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+/* Receive create shortcut */
+app.post("/create", function(req, res) {
+  console.log(JSON.stringify(req.body));
+});
 
 /* GET home page. */
 app.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+  res.render("index");
 });
 
 // error handler
@@ -37,135 +32,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
-// Amazon Alexa Handlers
-const LaunchRequestHandler = {
-  canHandle(handlerInput) {
-    return (
-      Alexa.getRequestType(handlerInput.requestEnvelope) === "LaunchRequest"
-    );
-  },
-  handle(handlerInput) {
-    const speakOutput =
-      "Welcome, you can say Hello or Help. Which would you like to try?";
-    return handlerInput.responseBuilder
-      .speak(speakOutput)
-      .reprompt(speakOutput)
-      .getResponse();
-  }
-};
-const HelloWorldIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
-      Alexa.getIntentName(handlerInput.requestEnvelope) === "HelloWorldIntent"
-    );
-  },
-  handle(handlerInput) {
-    const speakOutput = "Hello World!";
-    return (
-      handlerInput.responseBuilder
-        .speak(speakOutput)
-        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-        .getResponse()
-    );
-  }
-};
-const HelpIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
-      Alexa.getIntentName(handlerInput.requestEnvelope) === "AMAZON.HelpIntent"
-    );
-  },
-  handle(handlerInput) {
-    const speakOutput = "You can say hello to me! How can I help?";
-
-    return handlerInput.responseBuilder
-      .speak(speakOutput)
-      .reprompt(speakOutput)
-      .getResponse();
-  }
-};
-const CancelAndStopIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
-      (Alexa.getIntentName(handlerInput.requestEnvelope) ===
-        "AMAZON.CancelIntent" ||
-        Alexa.getIntentName(handlerInput.requestEnvelope) ===
-          "AMAZON.StopIntent")
-    );
-  },
-  handle(handlerInput) {
-    const speakOutput = "Goodbye!";
-    return handlerInput.responseBuilder.speak(speakOutput).getResponse();
-  }
-};
-const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    return (
-      Alexa.getRequestType(handlerInput.requestEnvelope) ===
-      "SessionEndedRequest"
-    );
-  },
-  handle(handlerInput) {
-    // Any cleanup logic goes here.
-    return handlerInput.responseBuilder.getResponse();
-  }
-};
-
-// The intent reflector is used for interaction model testing and debugging.
-const IntentReflectorHandler = {
-  canHandle(handlerInput) {
-    return (
-      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest"
-    );
-  },
-  handle(handlerInput) {
-    const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-    const speakOutput = `You just triggered ${intentName}`;
-
-    return (
-      handlerInput.responseBuilder
-        .speak(speakOutput)
-        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-        .getResponse()
-    );
-  }
-};
-
-// Generic error handling to capture any syntax or routing errors.
-const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`~~~~ Error handled: ${error.stack}`);
-    const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
-
-    return handlerInput.responseBuilder
-      .speak(speakOutput)
-      .reprompt(speakOutput)
-      .getResponse();
-  }
-};
-
-// Set up alexa skill
-const skillBuilder = Alexa.SkillBuilders.custom()
-  .addRequestHandlers(
-    LaunchRequestHandler,
-    HelloWorldIntentHandler,
-    HelpIntentHandler,
-    CancelAndStopIntentHandler,
-    SessionEndedRequestHandler,
-    IntentReflectorHandler
-  )
-  .addErrorHandlers(ErrorHandler);
-
-const skill = skillBuilder.create();
-
-const adapter = new ExpressAdapter(skill, true, true);
-app.post("/", adapter.getRequestHandlers());
 
 module.exports = app;
---
+++ database/server.js
@@ -0,0 +1,10 @@
+const mongo = require("mongodb").MongoClient;
+const url =
+  "mongodb+srv://howardwang:234Ar234@cluster0-vuff4.mongodb.net/test?retryWrites=true&w=majority";
+  
+/*
+              <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-text">for x in y</h5>
+                </div>
+              </div>*/
